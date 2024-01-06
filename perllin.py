51c00158b1705cab1ed7648ed245b6e99c9018b6
Gavin <zhenxin1017@gmail.com>
2024-01-06 17:06:29 +0000

    page_switch

--- perllin.py
+++ perllin.py
@@ -10,8 +10,15 @@ import math
 pygame.init()
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Perlin Noise Map")
+pygame.display.set_caption("PCG Game")
 clock = pygame.time.Clock()
+
+init_surface = pygame.Surface(screen.get_size())
+init_surface.fill((255, 255, 255))
+main_surface = pygame.Surface(screen.get_size())
+
+
+# init_surface.fill((255,255,255))
 
 
 class Player:
@@ -69,22 +76,6 @@ class Player:
             self.right_available = False
         else:
             self.right_available = True
-
-        # if self.check_collision(the_enemy):
-        #     if self.position[0] + self.rect.right >= the_enemy.x:
-        #         self.right_available = False
-        #     if self.position[0] <= the_enemy.x + the_enemy.rect.right:
-        #         self.left_available = False
-        #     if self.position[1] + self.rect.bottom >= the_enemy.y:
-        #         self.down_available = False
-        #     if self.position[1] <= the_enemy.y + the_enemy.rect.bottom:
-        #         self.top_available = False
-
-            # print("碰撞")
-
-
-
-
 
     def shooting_direction(self):
         mouse_x, mouse_y = pygame.mouse.get_pos()
@@ -321,6 +312,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                pygame.quit()
+                exit()
             # elif event.type == pygame.MOUSEBUTTONDOWN:
             #     if event.button == 1:  # 检测左键点击
             #         mouse_x, mouse_y = pygame.mouse.get_pos()
@@ -364,29 +357,64 @@ def main():
                     if enemy.hit(player.attack):  # 如果敌人被击败
                         enemies.remove(enemy)
 
-        screen.fill((0, 0, 0))
-        screen.blit(map, (0, 0))
-        screen.blit(player.pic,
-                    (player.position[0] - player.rect.right / 2, player.position[1] - player.rect.bottom / 2))
+        main_surface.fill((0, 0, 0))
+        main_surface.blit(map, (0, 0))
+        main_surface.blit(player.pic,
+                          (player.position[0] - player.rect.right / 2, player.position[1] - player.rect.bottom / 2))
         #
-        # screen.blit(pygame.transform.smoothscale(player.pic, (int(player.rect[2]), int(player.rect[3]))),
+        # main_surface.blit(pygame.transform.smoothscale(player.pic, (int(player.rect[2]), int(player.rect[3]))),
         #             (player.position[0] - player.rect.right / 2, player.position[1] - player.rect.bottom / 2))  # 放大或者缩小
         # 绘制子弹
         for bullet in player_bullets:
-            bullet.draw(screen)
+            bullet.draw(main_surface)
         # 绘制敌人
         for enemy in enemies:
-            screen.blit(enemy.pic,
-                        (enemy.x - enemy.rect.right / 2, enemy.y - enemy.rect.bottom / 2))
-            # screen.blit(pygame.transform.smoothscale(enemy.pic, (int(enemy.rect[2]/3), int(enemy.rect[3]/3))),
+            main_surface.blit(enemy.pic,
+                              (enemy.x - enemy.rect.right / 2, enemy.y - enemy.rect.bottom / 2))
+            # main_surface.blit(pygame.transform.smoothscale(enemy.pic, (int(enemy.rect[2]/3), int(enemy.rect[3]/3))),
             #             (enemy.x - enemy.rect.right / 2,
             #              enemy.y - enemy.rect.bottom / 2))  # 放大或者缩小
-            # enemy.draw(screen)
+            # enemy.draw(main_surface)
         # print((enemies[0].x - enemies[0].rect.right / 2, enemies[0].y - enemies[0].rect.bottom / 2))
+        screen.blit(main_surface, (0, 0))
         pygame.display.flip()
         clock.tick(60)
-    pygame.quit()
+
+    # exit()
+
+
+def init():
+    start_game = True
+    start = pygame.image.load("picture\\start.png")
+    finish = pygame.image.load("picture\\finish.png")
+    start_r = pygame.image.load("picture\\start_r.png")
+    finish_r = pygame.image.load("picture\\finish_r.png")
+    while start_game:
+        clock.tick(60)
+        buttons = pygame.mouse.get_pressed()
+        x1, y1 = pygame.mouse.get_pos()
+        if width * (3 / 8) <= x1 <= width * (5 / 8) and height * (5 / 10) <= y1 <= height * (6 / 10):
+            init_surface.blit(start_r, (width * (3 / 8), height * (5 / 10)))
+            if buttons[0]:
+                start_game = False
+        elif width * (3 / 8) <= x1 <= width * (5 / 8) and height * (7 / 10) <= y1 <= height * (8 / 10):
+            init_surface.blit(finish_r, (width * (3 / 8), height * (7 / 10)))
+            if buttons[0]:
+                pygame.quit()
+                exit()
+        else:
+            init_surface.blit(start, (width * (3 / 8), height * (5 / 10)))
+            init_surface.blit(finish, (width * (3 / 8), height * (7 / 10)))
+
+        screen.blit(init_surface, (0, 0))
+
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
 
 
 if __name__ == "__main__":
+    init()
     main()
